bbecf105d9f4cfff5ed62d3a846becbf32557b94
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-05-08 03:34:40 +0900

    Refactoring: simplify rollup.config.js

--- packages/express-engine-jsx/rollup.config.js
+++ packages/express-engine-jsx/rollup.config.js
@@ -6,37 +6,31 @@ import { terser } from 'rollup-plugin-terser';
 
 const extensions = ['.js', '.ts', '.tsx'];
 
-const config = (input) => {
-  return {
-    input,
-    output: {
-      dir: '.',
-      format: 'cjs',
-    },
-    plugins: [
-      external(),
-      resolve({
-        extensions,
-      }),
-      babel({
-        extensions,
-        exclude: /node_modules/,
-      }),
-      commonjs({
-        namedExports: {
-          'node_modules/react-dom/server.js': [
-            'renderToString',
-          ],
-        },
-      }),
-      (process.env.NODE_ENV === 'production' && terser()),
-    ],
-    external: [
-      'react-dom/server',
-    ],
-  };
+export default {
+  input: 'src/register.tsx',
+  output: {
+    dir: '.',
+    format: 'cjs',
+  },
+  plugins: [
+    external(),
+    resolve({
+      extensions,
+    }),
+    babel({
+      extensions,
+      exclude: /node_modules/,
+    }),
+    commonjs({
+      namedExports: {
+        'node_modules/react-dom/server.js': [
+          'renderToString',
+        ],
+      },
+    }),
+    (process.env.NODE_ENV === 'production' && terser()),
+  ],
+  external: [
+    'react-dom/server',
+  ],
 };
-
-export default [
-  config('src/register.tsx'),
-];
