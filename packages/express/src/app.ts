3f1340e0e17de53c6a2bca4d15b407f87466671d
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-05-10 04:19:48 +0900

    Extend express app.listen()

--- packages/express/src/app.ts
+++ packages/express/src/app.ts
@@ -21,9 +21,24 @@ export class ReactSsrExpress {
     }
 
     require(`@react-ssr/express-engine-${this.config.engine}`)(this.app, this.config);
+
+    this.app.listen = this.listen;
   }
 
   getApp(): Application {
     return this.app;
+  }
+
+  listen(port: number, hostname: string, backlog: number, callback?: Function): import('http').Server;
+  listen(port: number, hostname: string, callback?: Function): import('http').Server;
+  listen(port: number, callback?: Function): import('http').Server;
+  listen(path: string, callback?: Function): import('http').Server;
+  listen(handle: any, listeningListener?: Function): import('http').Server;
+  listen(port: any, hostname?: any, backlog?: any, callback?: any) {
+    for (let i = 0; i < this.app._router.stack.length; i++) {
+      const r = this.app._router.stack[i];
+      console.log(r);
+    }
+    return this.app.listen(port, hostname, backlog, callback);
   }
 }
