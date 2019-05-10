83400d18a88e591c4cd4c884139b36cea5baf7b6
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-05-11 00:06:36 +0900

    fix(11)

--- packages/express-engine-jsx/src/rollup.ts
+++ packages/express-engine-jsx/src/rollup.ts
@@ -6,9 +6,9 @@ import {
 import replace from 'rollup-plugin-replace';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
-// import commonjs from 'rollup-plugin-commonjs';
+import commonjs from 'rollup-plugin-commonjs';
 import virtual from 'rollup-plugin-virtual';
-import hypothetical from 'rollup-plugin-hypothetical';
+// import hypothetical from 'rollup-plugin-hypothetical';
 import { terser } from 'rollup-plugin-terser';
 
 const isProd: boolean = process.env.NODE_ENV === 'production';
