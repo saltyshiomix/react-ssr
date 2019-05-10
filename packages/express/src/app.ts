56fac44136a2ad7970b504da9ff47cd0b0222980
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-05-10 21:41:54 +0900

    Remove code comments

--- packages/express/src/app.ts
+++ packages/express/src/app.ts
@@ -5,12 +5,8 @@ export class ReactSsrExpress {
   private readonly app: Application;
   private readonly config: Config;
 
-  // private readonly originalListen: any;
-
   constructor(config: Config = {}) {
     this.app = express();
-    // this.originalListen = this.app.listen;
-
     this.config = {
       ...(new Config),
       ...config,
@@ -25,26 +21,9 @@ export class ReactSsrExpress {
     }
 
     require(`@react-ssr/express-engine-${this.config.engine}`)(this.app, this.config);
-
-    // this.app.listen = this.listen;
   }
 
   getApp(): Application {
     return this.app;
   }
-
-  // private listen(port: number, hostname: string, backlog: number, callback?: Function): import('http').Server;
-  // private listen(port: number, hostname: string, callback?: Function): import('http').Server;
-  // private listen(port: number, callback?: Function): import('http').Server;
-  // private listen(path: string, callback?: Function): import('http').Server;
-  // private listen(handle: any, listeningListener?: Function): import('http').Server;
-  // private listen(port: any, hostname?: any, backlog?: any, callback?: any) {
-  //   if (this.app._router) {
-  //     for (let i = 0; i < this.app._router.stack.length; i++) {
-  //       const r = this.app._router.stack[i];
-  //       console.log(r);
-  //     }
-  //   }
-  //   return this.originalListen(port, hostname, backlog, callback);
-  // }
 }
