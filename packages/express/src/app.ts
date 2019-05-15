e84ba80b1b758756ce74b50065fc1ad2410629ad
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-05-16 08:39:20 +0900

    Use function instead of class

--- packages/express/src/app.ts
+++ packages/express/src/app.ts
@@ -1,29 +1,23 @@
 import express, { Application } from 'express';
 import { Config } from './config';
 
-export class ReactSsrExpress {
-  private readonly app: Application;
-  private readonly config: Config;
+export function ReactSsrExpress(config: Config = {}) {
+  const app: Application = express();
 
-  constructor(config: Config = {}) {
-    this.app = express();
-    this.config = {
-      ...(new Config),
-      ...config,
-    };
+  config = {
+    ...(new Config),
+    ...config,
+  };
 
-    if (!this.config.engine) {
-      throw new Error('InvalidProgramException: view engine must be specified.');
-    }
-
-    if (!['jsx', 'tsx'].includes(this.config.engine)) {
-      throw new Error(`The engine ${this.config.engine} is not supported.`);
-    }
-
-    require(`@react-ssr/express-engine-${this.config.engine}`)(this.app, this.config);
+  if (!config.engine) {
+    throw new Error('InvalidProgramException: view engine must be specified.');
   }
 
-  getApp(): Application {
-    return this.app;
+  if (!['jsx', 'tsx'].includes(config.engine)) {
+    throw new Error(`The engine ${config.engine} is not supported.`);
   }
+
+  require(`@react-ssr/express-engine-${config.engine}`)(app, config);
+
+  return app;
 }
