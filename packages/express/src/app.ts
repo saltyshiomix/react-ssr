b63bf9158bc9debf01c69904291d325358b7b2a5
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-05-16 14:11:09 +0900

    debug

--- packages/express/src/app.ts
+++ packages/express/src/app.ts
@@ -1,16 +1,8 @@
 import express, { Application } from 'express';
-import delay from 'delay';
-import got from 'got';
+// import delay from 'delay';
+// import got from 'got';
 import { Config } from './config';
-import * as spinner from './spinner';
-
-let built: boolean = false;
-
-async function waitUntilBuilt() {
-  while (!built) {
-    await delay(300);
-  }
-}
+// import * as spinner from './spinner';
 
 export function ReactSsrExpress(config: Config = {}) {
   const app: Application = express();
@@ -31,34 +23,36 @@ export function ReactSsrExpress(config: Config = {}) {
   require(`@react-ssr/express-engine-${config.engine}`)(app, config);
 
   const _listen = app.listen;
+
+  // const buildSync = async (route: string) => {
+  //   let done: boolean = false;
+
+  //   try {
+  //     spinner.create(`Building '${route}'`);
+  //     await got(`http://localhost:8888${route}`);
+  //     done = true;
+  //   } catch (e) {
+  //     spinner.fail(e.response.body);
+  //     process.exit(1);
+  //   }
+
+  //   while (true) {
+  //     await delay(300);
+  //     if (done) {
+  //       break;
+  //     }
+  //   }
+  // };
+
   app.listen = (...args: any[]) => {
     if (args.length === 0) {
       throw new Error('1 - 4 arguments must be specified.');
     }
 
-    const server = _listen(8888, async () => {
-      const url = (route: string) => `http://localhost:8888${route}`;
-
-      console.log(app._router);
-
-      spinner.create(`Building '/'`);
-      await got(url('/'));
-
-      spinner.create(`Building '/' (test 2)`);
-      await got(url('/'));
-
-      spinner.create(`Building '/' (test 3)`);
-      await got(url('/'));
-
-      built = true;
-    });
-
-    waitUntilBuilt();
-
-    server.close();
+    console.log(app._router);
 
     const [port, ...rest] = args;
-    spinner.clear(`> Ready on http://localhost:${port}`);
+    // spinner.clear(`> Ready on http://localhost:${port}`);
 
     return _listen(port, ...rest);
   };
