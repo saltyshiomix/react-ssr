71f533613f1c4e9e65e4ed66aa567eae0a14765b
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-05-24 03:10:45 +0900

    Improve webpack config

--- packages/express/src/webpack.config.ts
+++ packages/express/src/webpack.config.ts
@@ -5,40 +5,58 @@ import { Configuration } from 'webpack';
 const cwd: string = process.cwd();
 const isProd: boolean = process.env.NODE_ENV === 'production';
 
-export default (name: string, distDir: string): Configuration => {
-  const babelRule = {
-    test: /\.(js|ts)x?$/,
-    exclude: /node_modules/,
+const babelRule = {
+  test: /\.(js|ts)x?$/,
+  exclude: /node_modules/,
+  use: {
     loader: 'babel-loader',
     options: {},
-  };
-  const hasBabelrc: boolean = existsSync(resolve(cwd, '.babelrc')) || existsSync(resolve(cwd, '.babelrc.js')) || existsSync(resolve(cwd, 'babel.config.js'));
-  const getBabelrc = () => {
-    if (existsSync(resolve(cwd, '.babelrc'))) {
-      return resolve(cwd, '.babelrc');
-    }
-    if (existsSync(resolve(cwd, '.babelrc.js'))) {
-      return resolve(cwd, '.babelrc.js');
-    }
-    return resolve(cwd, 'babel.config.js');
-  };
-
-  if (hasBabelrc) {
-    const babelrc: string = getBabelrc();
-    if (!isProd) {
-      console.log('[react-ssr] Babelrc in: ' + babelrc);
-    }
-    babelRule.options = {
-      extends: babelrc,
-    };
-  } else {
-    const { presets, plugins } = require(resolve(__dirname, '../babel'))();
-    babelRule.options = {
-      presets,
-      plugins,
-    };
+  },
+};
+const hasBabelrc: boolean = existsSync(resolve(cwd, '.babelrc')) || existsSync(resolve(cwd, '.babelrc.js')) || existsSync(resolve(cwd, 'babel.config.js'));
+const getBabelrc = () => {
+  if (existsSync(resolve(cwd, '.babelrc'))) {
+    return resolve(cwd, '.babelrc');
   }
+  if (existsSync(resolve(cwd, '.babelrc.js'))) {
+    return resolve(cwd, '.babelrc.js');
+  }
+  return resolve(cwd, 'babel.config.js');
+};
 
+if (hasBabelrc) {
+  const babelrc: string = getBabelrc();
+  !isProd && console.log('[react-ssr] Babelrc in: ' + babelrc);
+  babelRule.use.options = {
+    cacheDirectory: true,
+    extends: babelrc,
+  };
+} else {
+  babelRule.use.options = {
+    cacheDirectory: true,
+    presets: [
+      require('@babel/preset-env'),
+      require('@babel/preset-react'),
+      require('@babel/preset-typescript'),
+    ],
+    plugins: [
+      require('babel-plugin-react-require'),
+      require('@babel/plugin-syntax-dynamic-import'),
+      require('@babel/plugin-proposal-class-properties'),
+      [require('@babel/plugin-proposal-object-rest-spread'), {
+        useBuiltIns: true,
+      }],
+      [require('@babel/plugin-transform-runtime'), {
+        corejs: 2,
+        helpers: true,
+        regenerator: true,
+        useESModules: false,
+      }],
+    ],
+  };
+}
+
+export default (name: string, distDir: string): Configuration => {
   const config: Configuration = {
     mode: isProd ? 'production' : 'development',
     context: cwd,
