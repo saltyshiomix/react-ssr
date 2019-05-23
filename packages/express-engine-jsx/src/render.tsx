661bce23edd9894ad2001d422461de532e5d356e
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-05-23 21:54:45 +0900

    Test vol.fromJSON() method

--- packages/express-engine-jsx/src/render.tsx
+++ packages/express-engine-jsx/src/render.tsx
@@ -48,15 +48,19 @@ const render = (file: string, config: Config, props: any): string => {
 
   process.env.MEMFS_DONT_WARN = 'true';
   const fs = require('fs');
-  const { createFsFromVolume, Volume } = require('memfs');
+  const { Volume } = require('memfs');
   const { ufs } = require('unionfs');
 
   const vol = new Volume;
-  vol.mkdirSync('./react-ssr-src', { recursive: true });
-  vol.writeFileSync('./react-ssr-src/entry.js', entryContents, 'utf-8');
-  vol.writeFileSync('./react-ssr-src/page.js', pageContents, 'utf-8');
-  const mfs = createFsFromVolume(vol);
-  ufs.use(mfs).use(fs);
+  vol.fromJSON({
+    './react-ssr-src/entry.js': entryContents,
+    './react-ssr-src/page.js': pageContents,
+  }, cwd);
+  // vol.mkdirSync('./react-ssr-src', { recursive: true });
+  // vol.writeFileSync('./react-ssr-src/entry.js', entryContents, 'utf-8');
+  // vol.writeFileSync('./react-ssr-src/page.js', pageContents, 'utf-8');
+  // const mfs = createFsFromVolume(vol);
+  ufs.use(vol).use(fs);
 
   compiler.inputFileSystem = ufs;
   // compiler.resolvers.normal.fileSystem = compiler.inputFileSystem;
