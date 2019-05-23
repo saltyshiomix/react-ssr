b2d3bc3c64e1b840927d61a8aae4ed2b7efcf641
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-05-24 02:48:08 +0900

    Add @babel/preset-typescript

--- packages/express/babel.js
+++ packages/express/babel.js
@@ -3,6 +3,7 @@ module.exports = () => {
     presets: [
       require('@babel/preset-env'),
       require('@babel/preset-react'),
+      require('@babel/preset-typescript'),
     ],
     plugins: [
       require('babel-plugin-react-require'),
