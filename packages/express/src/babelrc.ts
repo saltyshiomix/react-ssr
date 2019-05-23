8a2f9a9ec2d9a7a6cfb6155e51b0cf028d7e57f1
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-05-24 03:56:23 +0900

    Fix the babel api usage

--- packages/express/src/babelrc.ts
+++ packages/express/src/babelrc.ts
@@ -3,9 +3,38 @@ import { resolve } from 'path';
 
 const cwd: string = process.cwd();
 
-export default (): string => {
+export default (): string|Function => {
   if (existsSync(resolve(cwd, '.babelrc'))) return resolve(cwd, '.babelrc');
   if (existsSync(resolve(cwd, '.babelrc.js'))) return resolve(cwd, '.babelrc.js');
   if (existsSync(resolve(cwd, 'babel.config.js'))) return resolve(cwd, 'babel.config.js');
-  return resolve(__dirname, '../babel.js');
+
+  return (api: any) => {
+    api.cache(true);
+
+    const presets = [
+      require('@babel/preset-env'),
+      require('@babel/preset-react'),
+      require('@babel/preset-typescript'),
+    ];
+
+    const plugins = [
+      require('babel-plugin-react-require'),
+      require('@babel/plugin-syntax-dynamic-import'),
+      require('@babel/plugin-proposal-class-properties'),
+      [require('@babel/plugin-proposal-object-rest-spread'), {
+        useBuiltIns: true,
+      }],
+      [require('@babel/plugin-transform-runtime'), {
+        corejs: 2,
+        helpers: true,
+        regenerator: true,
+        useESModules: false,
+      }],
+    ];
+
+    return {
+      presets,
+      plugins
+    };
+  }
 }
