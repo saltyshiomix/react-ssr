24ed197799df27a845c6fac3c707a3ac0df84ec3
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-05-24 04:01:02 +0900

    Fix extends path

--- packages/express/src/babelrc.ts
+++ packages/express/src/babelrc.ts
@@ -7,34 +7,5 @@ export default (): string|Function => {
   if (existsSync(resolve(cwd, '.babelrc'))) return resolve(cwd, '.babelrc');
   if (existsSync(resolve(cwd, '.babelrc.js'))) return resolve(cwd, '.babelrc.js');
   if (existsSync(resolve(cwd, 'babel.config.js'))) return resolve(cwd, 'babel.config.js');
-
-  return (api: any) => {
-    api.cache(true);
-
-    const presets = [
-      require('@babel/preset-env'),
-      require('@babel/preset-react'),
-      require('@babel/preset-typescript'),
-    ];
-
-    const plugins = [
-      require('babel-plugin-react-require'),
-      require('@babel/plugin-syntax-dynamic-import'),
-      require('@babel/plugin-proposal-class-properties'),
-      [require('@babel/plugin-proposal-object-rest-spread'), {
-        useBuiltIns: true,
-      }],
-      [require('@babel/plugin-transform-runtime'), {
-        corejs: 2,
-        helpers: true,
-        regenerator: true,
-        useESModules: false,
-      }],
-    ];
-
-    return {
-      presets,
-      plugins
-    };
-  }
+  return resolve(__dirname, '../babel.config.default.js');
 }
