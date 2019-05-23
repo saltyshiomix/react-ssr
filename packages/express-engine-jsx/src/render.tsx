edcbf27bb25808d9bc2d1c0cb2fb7460041fee9d
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-05-23 20:12:23 +0900

    Add compiler.resolvers

--- packages/express-engine-jsx/src/render.tsx
+++ packages/express-engine-jsx/src/render.tsx
@@ -18,6 +18,10 @@ import configure from './webpack.config';
 
 template.defaults.minimize = false;
 
+interface WebpackComplier extends webpack.Compiler {
+  resolvers?: any;
+}
+
 const getPagePath = (file: string, config: Config): string => {
   return file.split(sep + config.viewsDir + sep)[1];
 };
@@ -38,7 +42,7 @@ const render = async (file: string, config: Config, props: any): Promise<string>
   const name: string = basename(page).replace('.jsx', '');
   const entryContents: string = template(resolve(__dirname, 'page.jsx'), { props });
   const pageContents: string = template(file, props);
-  const compiler: webpack.Compiler = webpack(configure(name, distDir));
+  const compiler: WebpackComplier = webpack(configure(name, distDir));
 
   const fs = require('fs');
   const { fs: mfs } = require('memfs');
@@ -51,6 +55,7 @@ const render = async (file: string, config: Config, props: any): Promise<string>
 
   compiler.inputFileSystem = ufs;
   compiler.outputFileSystem = fs;
+  compiler.resolvers.normal.fileSystem = ufs;
 
   try {
     compiler.run((err, stats) => {
