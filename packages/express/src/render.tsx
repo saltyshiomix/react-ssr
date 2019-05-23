37fd11818c72d8761e039e1584e61ec11c196550
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-05-24 04:53:27 +0900

    Output cache html

--- packages/express/src/render.tsx
+++ packages/express/src/render.tsx
@@ -86,7 +86,7 @@ const render = async (file: string, config: Config, props: any): Promise<string>
     return html;
 
   } finally {
-    // await outputFileSync(cache, html);
+    await outputFileSync(cache, html);
   }
 };
 
