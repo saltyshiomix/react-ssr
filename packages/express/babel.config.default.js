2b1751e945fbcabefd973d4e6968b3c3156ee29f
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-08-30 19:27:34 +0900

    Refactoring: add comma

--- packages/express/babel.config.default.js
+++ packages/express/babel.config.default.js
@@ -24,6 +24,6 @@ module.exports = (api) => {
 
   return {
     presets,
-    plugins
+    plugins,
   };
 }
--- packages/express/babel.config.js
+++ packages/express/babel.config.js
@@ -13,6 +13,6 @@ module.exports = (api) => {
 
   return {
     presets,
-    plugins
+    plugins,
   };
 }
