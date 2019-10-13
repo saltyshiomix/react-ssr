75ed96e91832037ff62b7cf3216123aa3bfbd6e2
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-14 05:58:32 +0900

    Override express.application.listen

--- packages/express/lib/app.ts
+++ packages/express/lib/app.ts
@@ -1,3 +1,4 @@
+import { Server } from 'http';
 import express from 'express';
 import register from './register';
 import { Config } from './config';
@@ -12,6 +13,19 @@ const ctor = (config?: Config): express.Express => {
   const app: express.Express = express();
 
   register(app, Object.assign(new Config, config));
+
+  const _listen = express.application.listen;
+  function listen(port: number, hostname: string, backlog: number, callback?: (...args: any[]) => void): Server;
+  function listen(port: number, hostname: string, callback?: (...args: any[]) => void): Server;
+  function listen(port: number, callback?: (...args: any[]) => void): Server;
+  function listen(callback?: (...args: any[]) => void): Server;
+  function listen(path: string, callback?: (...args: any[]) => void): Server;
+  function listen(handle: any, listeningListener?: () => void): Server;
+  function listen(...args: any[]): Server {
+    console.log('Optimizing performance...');
+    return _listen(...args);
+  }
+  app.listen = listen;
 
   return app;
 };
