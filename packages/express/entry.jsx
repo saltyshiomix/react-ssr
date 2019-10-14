56351f0c1047f483db599cc9d708e3cf3e33c8b7
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-14 23:59:10 +0900

    Fix "Page is not defined"

--- packages/express/entry.jsx
+++ packages/express/entry.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Page from './page';
 
+let App = Page;
 if ('__REACT_SSR_DEVELOPMENT__') {
   const { hot } = require('react-hot-loader/root');
-  Page = hot(Page);
+  App = hot(Page);
 }
 
 const props = JSON.parse('__REACT_SSR__');
 
-ReactDOM.hydrate(<Page {...props} />, document.getElementById('app'));
+ReactDOM.hydrate(<App {...props} />, document.getElementById('app'));
