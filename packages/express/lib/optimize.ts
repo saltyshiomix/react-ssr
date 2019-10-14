0cab017d73e80e3d0137fb073ee4c81ce15b128e
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-15 07:13:06 +0900

    debug

--- packages/express/lib/optimize.ts
+++ packages/express/lib/optimize.ts
@@ -126,7 +126,11 @@ export default async (app: express.Application, server: http.Server, config: Con
     const hash = hasha(env + page, { algorithm: 'md5' });
     const filename = path.join(cwd, config.cacheDir, `${hash}.js`);
 
-    console.log(require.cache[page]);
+    console.log(page)
+    console.log('')
+
+    console.log(require.cache);
+
     delete require.cache[page];
 
     await waitUntilCompleted(mfs, filename);
