8ff09cf947e1e26fd830ae5b1eff36646840b33f
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-15 16:36:08 +0900

    Fix "file not found" exception

--- packages/express/lib/optimize.ts
+++ packages/express/lib/optimize.ts
@@ -5,6 +5,7 @@ import net from 'net';
 import http from 'http';
 import express from 'express';
 import hasha from 'hasha';
+import readdir from 'recursive-readdir';
 import webpack from 'webpack';
 import configure from './webpack.config';
 import Config from './config';
@@ -18,21 +19,47 @@ const cwd = process.cwd();
 const ext = '.' + getEngine();
 const codec = require('json-url')('lzw');
 
-const getPages = (dir: string): string[] => {
-  const pages: string[] = [];
-  const dirents = fs.readdirSync(dir, { withFileTypes: true });
-  for (const dirent of dirents) {
-    const p = path.join(dir, dirent.name);
-    if (dirent.isDirectory()) {
-      pages.push(...(getPages(p)));
-    } else {
-      pages.push(p);
-    }
-  }
-  return pages;
+const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+const ignores = [
+  '.*',
+  '*.json',
+  '*.lock',
+  'LICENSE',
+];
+
+const ignoreDotDir = (file: string, stats: fse.Stats) => {
+  return stats.isDirectory() && path.basename(file).startsWith('.');
 };
 
-const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+const ignoreNodeModules = (file: string, stats: fse.Stats) => {
+  return stats.isDirectory() && path.basename(file) == 'node_modules';
+};
+
+const getPages = async (config: Config): Promise<string[][]> => {
+  const allPages = await readdir(cwd, [ignoreNodeModules, ignoreDotDir, ...ignores]);
+  const entryPages = await readdir(path.join(cwd, config.viewsDir), [ignoreDotDir, ...ignores]);
+  const otherPages = [];
+  for (let i = 0; i < allPages.length; i++) {
+    const p = allPages[i];
+    if (entryPages.includes(p)) {
+      continue;
+    }
+    otherPages.push(p);
+  }
+
+  console.log(otherPages);
+
+  return [entryPages, otherPages];
+};
+
+const getRelativeInfo = (file: string): string[] => {
+  const splitted = file.replace(cwd, '').split(path.sep);
+  const [, ...rest] = splitted;
+  const relativeFile = rest.join(path.sep);
+  const relativeDir = path.dirname(relativeFile);
+  return [relativeFile, relativeDir];
+};
 
 const waitUntilCompleted = async (mfs: any, filename: string) => {
   const existsInMFS = mfs.existsSync(filename);
@@ -47,11 +74,92 @@ const waitUntilCompleted = async (mfs: any, filename: string) => {
   waitUntilCompleted(mfs, filename);
 }
 
-const { ufs } = require('unionfs');
-const MemoryFileSystem = require('memory-fs');
-const mfs = new MemoryFileSystem;
-ufs.use(mfs).use(fs);
-mfs.mkdirpSync(path.join(cwd, 'react-ssr-src'));
+// onchange bundling
+async function bundle(config: Config, ufs: any, mfs: any): Promise<void>;
+
+// initial bundling
+async function bundle(config: Config, ufs: any, mfs: any, app: express.Application): Promise<void>;
+
+async function bundle(config: Config, ufs: any, mfs: any, app?: express.Application) {
+  const entry: webpack.Entry = {};
+  const [entryPages, otherPages] = await getPages(config);
+  const template = fse.readFileSync(path.join(__dirname, '../entry.jsx'))
+                      .toString()
+                      .replace(
+                        '\'__REACT_SSR_DEVELOPMENT__\'',
+                        env === 'development' ? 'true' : 'false',
+                      );
+
+  for (let i = 0; i < entryPages.length; i++) {
+    const page = entryPages[i];
+    const hash = hasha(env + page, { algorithm: 'md5' });
+    const [filename, dirname] = getRelativeInfo(page);
+    mfs.mkdirpSync(path.join(cwd, `react-ssr-src/${dirname}`));
+    mfs.writeFileSync(path.join(cwd, `react-ssr-src/${dirname}/entry-${path.basename(filename)}`), template);
+    mfs.writeFileSync(path.join(cwd, `react-ssr-src/${filename}`), fse.readFileSync(page));
+    entry[hash] = `react-ssr-src/${dirname}/entry-${path.basename(filename)}`;
+  }
+
+  for (let i = 0; i < otherPages.length; i++) {
+    const page = otherPages[i];
+    const [filename, dirname] = getRelativeInfo(page);
+    mfs.mkdirpSync(path.join(cwd, `react-ssr-src/${dirname}`));
+    mfs.writeFileSync(path.join(cwd, `react-ssr-src/${filename}`), fse.readFileSync(page));
+  }
+
+  const webpackConfig: webpack.Configuration = configure(entry, config.cacheDir);
+  const compiler: webpack.Compiler = webpack(webpackConfig);
+  compiler.inputFileSystem = ufs;
+  compiler.outputFileSystem = mfs;
+  compiler.run((err: Error) => {
+    err && console.error(err.stack || err);
+  });
+
+  if (app) {
+    for (let i = 0; i < entryPages.length; i++) {
+      const page = entryPages[i];
+      const hash = hasha(env + page, { algorithm: 'md5' });
+      const filename = path.join(cwd, config.cacheDir, env, `${hash}.js`);
+  
+      await waitUntilCompleted(mfs, filename);
+  
+      const [, ...rest] = page.replace(cwd, '').split(path.sep);
+      const id = rest.join('/');
+      const route = '/_react-ssr/' + id.replace(ext, '.js');
+  
+      console.log(`  [ ok ] optimized "${id}"`);
+  
+      app.get(route, async (req, res) => {
+        const props = await codec.decompress(req.query.props);
+        if (env === 'development') {
+          console.log('[ info ] the props below is rendered from the server side');
+          console.log(props);
+        }
+  
+        const swichableFS = env === 'development' ? mfs : fse;
+        const script = swichableFS.readFileSync(filename)
+                                  .toString()
+                                  .replace(
+                                    '__REACT_SSR__',
+                                    JSON.stringify(props).replace(/"/g, '\\"'),
+                                  );
+  
+        res.type('.js');
+        res.send(script);
+      });
+    }
+  } else {
+    for (let i = 0; i < entryPages.length; i++) {
+      const page = entryPages[i];
+      const hash = hasha(env + page, { algorithm: 'md5' });
+      const filename = path.join(cwd, config.cacheDir, env, `${hash}.js`);
+      await waitUntilCompleted(mfs, filename);
+    }
+  }
+
+  // wait until bundled files are ready
+  await sleep(1500);
+};
 
 export default async (app: express.Application, server: http.Server, config: Config): Promise<http.Server> => {
   let reloadable: any = false;
@@ -66,59 +174,13 @@ export default async (app: express.Application, server: http.Server, config: Con
 
   console.log('[ info ] optimizing for the performance...');
 
-  const entry: webpack.Entry = {};
-  const pages = getPages(path.join(cwd, config.viewsDir));
+  const { ufs } = require('unionfs');
+  const MemoryFileSystem = require('memory-fs');
+  const mfs = new MemoryFileSystem;
+  ufs.use(mfs).use(fs);
+  mfs.mkdirpSync(path.join(cwd, 'react-ssr-src'));
 
-  for (let i = 0; i < pages.length; i++) {
-    const page = pages[i];
-    const hash = hasha(env + page, { algorithm: 'md5' });
-    mfs.mkdirpSync(path.join(cwd, `react-ssr-src/${hash}`));
-    let entryFile = fse.readFileSync(path.join(__dirname, '../entry.jsx')).toString();
-    entryFile = entryFile.replace('\'__REACT_SSR_DEVELOPMENT__\'', env === 'development' ? 'true' : 'false');
-    mfs.writeFileSync(path.join(cwd, `react-ssr-src/${hash}/entry${ext}`), entryFile);
-    mfs.writeFileSync(path.join(cwd, `react-ssr-src/${hash}/page${ext}`), fse.readFileSync(page));
-    entry[hash] = `./react-ssr-src/${hash}/entry${ext}`;
-  }
-
-  const webpackConfig: webpack.Configuration = configure(entry, config.cacheDir);
-  const compiler: webpack.Compiler = webpack(webpackConfig);
-  compiler.inputFileSystem = ufs;
-  compiler.outputFileSystem = mfs;
-  compiler.run((err: Error) => {
-    err && console.error(err.stack || err);
-  });
-
-  for (let i = 0; i < pages.length; i++) {
-    const page = pages[i];
-    const hash = hasha(env + page, { algorithm: 'md5' });
-    const filename = path.join(cwd, config.cacheDir, env, `${hash}.js`);
-
-    await waitUntilCompleted(mfs, filename);
-
-    const [, ...rest] = page.replace(cwd, '').split(path.sep);
-    const id = rest.join('/');
-    const route = '/_react-ssr/' + id.replace(ext, '.js');
-
-    console.log(`  [ ok ] optimized "${id}"`);
-
-    app.get(route, async (req, res) => {
-      const props = await codec.decompress(req.query.props);
-      if (env === 'development') {
-        console.log('[ info ] the props below is rendered from the server side');
-        console.log(props);
-      }
-
-      const swichableFS = env === 'development' ? mfs : fse;
-      const script = swichableFS.readFileSync(filename).toString()
-                      .replace(
-                        '__REACT_SSR__',
-                        JSON.stringify(props).replace(/"/g, '\\"'),
-                      );
-
-      res.type('.js');
-      res.send(script);
-    });
-  }
+  await bundle(config, ufs, mfs, app);
 
   if (env === 'development') {
     const escaperegexp = require('lodash.escaperegexp');
@@ -138,40 +200,9 @@ export default async (app: express.Application, server: http.Server, config: Con
     process.on('exit', closeWatching);
 
     watcher.on('change', async (p: string) => {
-      // remove file cache by operating system
       await fse.remove(path.join(cwd, config.cacheDir));
-
-      for (let i = 0; i < pages.length; i++) {
-        const page = pages[i];
-        const hash = hasha(env + page, { algorithm: 'md5' });
-        mfs.mkdirpSync(path.join(cwd, `react-ssr-src/${hash}`));
-        let entryFile = fse.readFileSync(path.join(__dirname, '../entry.jsx')).toString();
-        entryFile = entryFile.replace('\'__REACT_SSR_DEVELOPMENT__\'', 'true');
-        mfs.writeFileSync(path.join(cwd, `react-ssr-src/${hash}/entry${ext}`), entryFile);
-        mfs.writeFileSync(path.join(cwd, `react-ssr-src/${hash}/page${ext}`), fse.readFileSync(page));
-        entry[hash] = `./react-ssr-src/${hash}/entry${ext}`;
-      }
-
-      const webpackConfig: webpack.Configuration = configure(entry, config.cacheDir);
-      const compiler: webpack.Compiler = webpack(webpackConfig);
-      compiler.inputFileSystem = ufs;
-      compiler.outputFileSystem = mfs;
-      compiler.run((err: Error) => {
-        err && console.error(err.stack || err);
-      });
-
-      for (let i = 0; i < pages.length; i++) {
-        const page = pages[i];
-        const hash = hasha(env + page, { algorithm: 'md5' });
-        const filename = path.join(cwd, config.cacheDir, env, `${hash}.js`);
-        await waitUntilCompleted(mfs, filename);
-      }
-
-      // wait until bundled files are ready
-      await sleep(1500);
-
+      await bundle(config, ufs, mfs);
       reloadable.reload();
-
       console.log('[ info ] reloaded');
     });
 
