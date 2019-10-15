58e4d2c7a3d9043cf5e7def467cf7dae5dc61e6e
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-15 17:08:45 +0900

    Fix unsaved files

--- packages/express/entry.jsx
+++ packages/express/entry.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Page from './page';
+import Page from './__REACT_SSR_PAGE_NAME_';
 
 let renderMethod = ReactDOM.hydrate;
 
@@ -8,7 +8,7 @@ if ('__REACT_SSR_DEVELOPMENT__') {
   renderMethod = ReactDOM.render;
 }
 
-const props = JSON.parse('__REACT_SSR__');
+const props = JSON.parse('__REACT_SSR_PROPS__');
 
 renderMethod((
   <React.Fragment>
