f9909124244d743b147777b287678d80f7550533
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-15 09:19:02 +0900

    Stop webpack.compiler.run() when development mode

--- packages/express/lib/optimize.ts
+++ packages/express/lib/optimize.ts
@@ -135,7 +135,7 @@ export default async (app: express.Application, server: http.Server, config: Con
     process.on('SIGTERM', closeWatching);
     process.on('exit', closeWatching);
 
-    watcher.on('change', (p: string) => {
+    watcher.on('change', async (p: string) => {
       // remove file cache by operating system
       fse.removeSync(path.join(cwd, config.cacheDir, env));
 
@@ -147,20 +147,27 @@ export default async (app: express.Application, server: http.Server, config: Con
         // entry[hash] = ['webpack-hot-middleware/client', `./react-ssr-src/${hash}/entry${ext}`];
       }
 
+      for (let i = 0; i < pages.length; i++) {
+        const page = pages[i];
+        const hash = hasha(env + page, { algorithm: 'md5' });
+        const filename = path.join(cwd, config.cacheDir, env, `${hash}.js`);
+        await waitUntilCompleted(mfs, filename);
+      }
+      console.log('[ info ] recompiled all bundles');
       // const webpackConfig: webpack.Configuration = configure(entry, config.cacheDir);
       // const compiler: webpack.Compiler = webpack(webpackConfig);
       // compiler.inputFileSystem = ufs;
       // compiler.outputFileSystem = mfs;
-      compiler.run(async (err: Error) => {
-        err && console.error(err.stack || err);
-        for (let i = 0; i < pages.length; i++) {
-          const page = pages[i];
-          const hash = hasha(env + page, { algorithm: 'md5' });
-          const filename = path.join(cwd, config.cacheDir, env, `${hash}.js`);
-          await waitUntilCompleted(mfs, filename);
-        }
-        console.log('[ info ] recompiled all bundles');
-      });
+      // compiler.run(async (err: Error) => {
+      //   err && console.error(err.stack || err);
+      //   for (let i = 0; i < pages.length; i++) {
+      //     const page = pages[i];
+      //     const hash = hasha(env + page, { algorithm: 'md5' });
+      //     const filename = path.join(cwd, config.cacheDir, env, `${hash}.js`);
+      //     await waitUntilCompleted(mfs, filename);
+      //   }
+      //   console.log('[ info ] recompiled all bundles');
+      // });
     });
 
     console.log('[ info ] running a server (development mode)');
