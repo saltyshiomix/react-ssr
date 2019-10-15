56bce6071c0b6af85e2cc9cd174e2e159ca90648
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-15 09:56:31 +0900

    test

--- packages/express/lib/webpack.config.ts
+++ packages/express/lib/webpack.config.ts
@@ -31,7 +31,7 @@ export default (entry: webpack.Entry, cacheDir: string): webpack.Configuration =
     entry,
     output: {
       path: path.join(cwd, cacheDir, env),
-      publicPath: `/_react-ssr/`,
+      publicPath: `/_react-ssr/views/`,
       filename: '[name].js',
     },
     resolve: {
