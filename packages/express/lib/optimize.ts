50543369545aa015892f1a85fef4c49ea1b031df
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-15 09:50:47 +0900

    test

--- packages/express/lib/optimize.ts
+++ packages/express/lib/optimize.ts
@@ -161,16 +161,16 @@ export default async (app: express.Application, server: http.Server, config: Con
       // app.use(require('webpack-hot-middleware')(compiler, {
       //   reload: true,
       // }));
-      // compiler.run(async (err: Error) => {
-      //   err && console.error(err.stack || err);
-      //   for (let i = 0; i < pages.length; i++) {
-      //     const page = pages[i];
-      //     const hash = hasha(env + page, { algorithm: 'md5' });
-      //     const filename = path.join(cwd, config.cacheDir, env, `${hash}.js`);
-      //     await waitUntilCompleted(mfs, filename);
-      //   }
-      //   console.log('[ info ] recompiled all bundles');
-      // });
+      compiler.run(async (err: Error) => {
+        err && console.error(err.stack || err);
+        for (let i = 0; i < pages.length; i++) {
+          const page = pages[i];
+          const hash = hasha(env + page, { algorithm: 'md5' });
+          const filename = path.join(cwd, config.cacheDir, env, `${hash}.js`);
+          await waitUntilCompleted(mfs, filename);
+        }
+        console.log('[ info ] recompiled all bundles');
+      });
     });
 
     console.log('[ info ] running a server (development mode)');
--- packages/express/lib/webpack.config.ts
+++ packages/express/lib/webpack.config.ts
@@ -22,12 +22,16 @@ export default (entry: webpack.Entry, cacheDir: string): webpack.Configuration =
     plugins.push(new webpack.HotModuleReplacementPlugin());
   }
 
+  // const [, ...rest] = file.replace(process.cwd(), '').replace(ext, '.js').split(path.sep);
+  // const route: string = '/_react-ssr/' + rest.join('/');
+
   return {
     mode: env,
     context: cwd,
     entry,
     output: {
       path: path.join(cwd, cacheDir, env),
+      publicPath: `/_react-ssr/`,
       filename: '[name].js',
     },
     resolve: {
