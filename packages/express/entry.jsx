a28bad74bf4a3b0078d62296735fc691b58c2aff
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-15 13:33:05 +0900

    Refactoring: rename

--- packages/express/entry.jsx
+++ packages/express/entry.jsx
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Page from './page';
 
-let mount = ReactDOM.hydrate;
+let renderMethod = ReactDOM.hydrate;
 
 if ('__REACT_SSR_DEVELOPMENT__') {
-  mount = ReactDOM.render;
+  renderMethod = ReactDOM.render;
   require('reload/lib/reload');
 }
 
 const props = JSON.parse('__REACT_SSR__');
 
-mount((
+renderMethod((
   <React.Fragment>
     <Page {...props} />
     {process.env.NODE_ENV === 'production' ? null : <script src="/reload/reload.js"></script>}
