ffc5c697da1ee5d4b9babf736aae276f5d1f0ec6
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-15 13:31:15 +0900

    Use ReactDOM.render when development mode

--- packages/express/entry.jsx
+++ packages/express/entry.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Page from './page';
 
+let mount = ReactDOM.hydrate;
+
 if ('__REACT_SSR_DEVELOPMENT__') {
+  mount = ReactDOM.render;
   require('reload/lib/reload');
 }
 
 const props = JSON.parse('__REACT_SSR__');
 
-ReactDOM.hydrate((
+mount((
   <React.Fragment>
     <Page {...props} />
     {process.env.NODE_ENV === 'production' ? null : <script src="/reload/reload.js"></script>}
