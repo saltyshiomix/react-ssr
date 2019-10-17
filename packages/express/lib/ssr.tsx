2bd2237f53e8f6a23f8c458aaaa1d10a139b9891
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-17 17:24:46 +0900

    test

--- packages/express/lib/ssr.tsx
+++ packages/express/lib/ssr.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
-import ReactHtmlParser from 'react-html-parser';
-import cheerio from 'cheerio';
+// import ReactHtmlParser from 'react-html-parser';
+// import cheerio from 'cheerio';
 
 interface SsrProps {
   children: any;
@@ -41,30 +41,4 @@ export default (props: SsrProps) => {
       {React.cloneElement(children, { script })}
     </React.Fragment>
   );
-
-  // const $ = cheerio.load(html);
-  // const htmlAttr = $('html').attr();
-  // const bodyAttr = $('body').attr();
-  // const head = $('head').html();
-  // const body = $('body').html();
-
-  // return (
-  //   <html {...htmlAttr}>
-  //     <head>
-  //       {head ? ReactHtmlParser(head) : null}
-  //     </head>
-  //     <body
-  //       data-ssr-id={ssrId}
-  //       {...bodyAttr}
-  //     >
-  //       <div id="react-ssr-root">
-  //         {body ? ReactHtmlParser(body) : null}
-  //       </div>
-  //       <div id="react-ssr-script">
-  //         <script src={script}></script>
-  //         {process.env.NODE_ENV === 'production' ? null : <script src="/reload/reload.js"></script>}
-  //       </div>
-  //     </body>
-  //   </html>
-  // );
 };
