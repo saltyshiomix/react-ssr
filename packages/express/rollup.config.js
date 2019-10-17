8853356bb09a8104e6d3f271ad505fc9d4ccfd5d
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-17 17:36:00 +0900

    Fix bugs

--- packages/express/rollup.config.js
+++ packages/express/rollup.config.js
@@ -6,31 +6,52 @@ import { terser } from 'rollup-plugin-terser';
 
 const extensions = ['.js', '.ts', '.tsx'];
 
-export default {
-  input: 'lib/index.ts',
-  output: {
-    dir: 'dist',
-    format: 'cjs',
+export default [
+  {
+    input: 'lib/core/index.ts',
+    output: {
+      file: 'dist/index.js',
+      format: 'cjs',
+    },
+    plugins: [
+      external(),
+      resolve({
+        extensions,
+      }),
+      babel({
+        extensions,
+        exclude: /node_modules/,
+      }),
+      commonjs({
+        namedExports: {
+          'node_modules/react-dom/server.js': [
+            'renderToString',
+          ],
+        },
+      }),
+      (process.env.NODE_ENV === 'production' && terser()),
+    ],
+    external: [
+      'react-dom/server',
+    ],
   },
-  plugins: [
-    external(),
-    resolve({
-      extensions,
-    }),
-    babel({
-      extensions,
-      exclude: /node_modules/,
-    }),
-    commonjs({
-      namedExports: {
-        'node_modules/react-dom/server.js': [
-          'renderToString',
-        ],
-      },
-    }),
-    (process.env.NODE_ENV === 'production' && terser()),
-  ],
-  external: [
-    'react-dom/server',
-  ],
-};
+  {
+    input: 'lib/script.tsx',
+    output: {
+      file: 'dist/script.js',
+      format: 'cjs',
+    },
+    plugins: [
+      external(),
+      resolve({
+        extensions,
+      }),
+      babel({
+        extensions,
+        exclude: /node_modules/,
+      }),
+      commonjs(),
+      (process.env.NODE_ENV === 'production' && terser()),
+    ],
+  }
+];
--- packages/express/script.js
+++ packages/express/script.js
@@ -1,11 +1,3 @@
-import React from 'react';
-
-export default (props) => {
-  return (
-    <React.Fragment>
-      {props.children}
-      <script src={props.script}></script>
-      {process.env.NODE_ENV === 'production' ? null : <script src="/reload/reload.js"></script>}
-    </React.Fragment>
-  );
-};
+const s = require('./dist/script');
+module.exports = s;
+module.exports.default = s;
