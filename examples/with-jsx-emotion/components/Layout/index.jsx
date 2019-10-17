212e0b40fce1b7c96332a5b2e0940b03a930b104
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-18 07:55:15 +0900

    Update examples/with-jsx-emotion

--- examples/with-jsx-emotion/components/Layout/index.jsx
+++ examples/with-jsx-emotion/components/Layout/index.jsx
@@ -1,4 +1,5 @@
 import ReactSsrScript from '@react-ssr/express/script';
+import { css, Global } from '@emotion/core';
 
 export const Layout = (props) => {
   const {
@@ -11,9 +12,28 @@ export const Layout = (props) => {
     <html lang="en">
       <head>
         <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content={'user-scalable=0, initial-scale=1, minimum-scale=1, width=device-width, height=device-height'} />
       </head>
+      <Global
+        styles={css`
+          html, body {
+            margin: 0;
+            padding: 0;
+            min-height: 100%;
+          }
+          body {
+            padding: 2rem 4rem;
+            background: papayawhip;
+            font-family: Helvetica, Arial, sans-serif;
+            font-size: 24px;
+          }
+        `}
+      />
       <body data-ssr-id='emotion'>
-        {children}
+        <div className="container">
+          {children}
+        </div>
         <ReactSsrScript script={script} />
       </body>
     </html>
