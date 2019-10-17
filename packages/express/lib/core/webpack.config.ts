ee8252e338125500c0a6a79e4d9baa004011dc1a
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-18 00:56:36 +0900

    Add custom webpack configuration

--- packages/express/lib/core/webpack.config.ts
+++ packages/express/lib/core/webpack.config.ts
@@ -1,5 +1,7 @@
 import path from 'path';
 import webpack from 'webpack';
+import { smart as merge } from 'webpack-merge';
+import TerserPlugin from 'terser-webpack-plugin';
 import {
   hasUserBabelrc,
   getBabelrc,
@@ -9,6 +11,40 @@ import {
 const cwd = process.cwd();
 const env = process.env.NODE_ENV === 'production' ? 'production' : 'development';
 
+const prodConfig: webpack.Configuration = {
+  performance: {
+    hints: 'warning',
+  },
+  output: {
+    pathinfo: false,
+  },
+  optimization: {
+    namedModules: false,
+    namedChunks: false,
+    nodeEnv: 'production',
+    flagIncludedChunks: true,
+    occurrenceOrder: true,
+    sideEffects: true,
+    usedExports: true,
+    concatenateModules: true,
+    splitChunks: {
+      minSize: 30000,
+      maxAsyncRequests: 5,
+      maxInitialRequests: 3,
+    },
+    noEmitOnErrors: true,
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('production'),
+    }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
+};
+
 export default (entry: webpack.Entry, cacheDir: string): webpack.Configuration => {
   if (env === 'development') {
     if (hasUserBabelrc()) {
@@ -16,17 +52,12 @@ export default (entry: webpack.Entry, cacheDir: string): webpack.Configuration =
     }
   }
 
-  console.log(env);
-  console.log(path.join(cwd, cacheDir, env));
-  console.log(entry);
-
-  return {
-    mode: env,
+  let config: webpack.Configuration = {
+    mode: 'development',
     context: cwd,
     entry,
     output: {
-      // path: path.join(cwd, cacheDir, env),
-      path: path.resolve(cwd, cacheDir, env),
+      path: path.join(cwd, cacheDir, env),
       filename: '[name].js',
     },
     resolve: {
@@ -38,4 +69,10 @@ export default (entry: webpack.Entry, cacheDir: string): webpack.Configuration =
       ],
     },
   };
+
+  if (env === 'production') {
+    config = merge(config, prodConfig);
+  }
+
+  return config;
 };
