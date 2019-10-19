0b02ac03b72696b4195eede5ec22effc7a9e165e
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-20 00:38:50 +0900

    Refactoring

--- packages/express/lib/script.tsx
+++ packages/express/lib/script.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 export default ({ script }: { script: string }) => {
   const mountable = typeof window === 'undefined';
   const reloadable = process.env.NODE_ENV !== 'production';
+
   return (
     <React.Fragment>
       {mountable ? <script id="react-ssr-script" src={script}></script> : null}
