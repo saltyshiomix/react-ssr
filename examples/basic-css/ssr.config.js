944e1d5c292870896a3ea815ca655a8a61f519f0
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-19 15:05:47 +0900

    Update examples/basic-css

--- examples/basic-css/ssr.config.js
+++ examples/basic-css/ssr.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-  webpack: (config, env) => Object.assign(config, {
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: [
-            'style-loader',
-            'css-loader',
-          ],
-        },
-      ],
-    },
-  }),
+  webpack: (config, env) => {
+    config.module.rules = [
+      ...(config.module.rules),
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ];
+    return config;
+  },
 };
