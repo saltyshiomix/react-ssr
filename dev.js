84fe81a4f1ca5a9dbbf7dc00bf234a262bd79b8c
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-19 14:23:56 +0900

    Refactoring

--- dev.js
+++ dev.js
@@ -1,27 +1,27 @@
-const { existsSync, copy, remove } = require('fs-extra');
+const {
+  existsSync,
+  copySync,
+  removeSync,
+} = require('fs-extra');
 const { resolve } = require('path');
 const { execSync } = require('child_process');
 const chalk = require('chalk');
 
-async function dev() {
-  let example = 'basic-jsx';
-  if (3 <= process.argv.length) {
-    const newExample = process.argv[2];
-    if (!existsSync(resolve(__dirname, `examples/${newExample}`))) {
-      console.log(chalk.red(`Not found examples/${newExample}`));
-      console.log('');
-      process.exit(1);
-    }
-    example = newExample;
+let example = 'basic-jsx';
+if (3 <= process.argv.length) {
+  const newExample = process.argv[2];
+  if (!existsSync(resolve(__dirname, `examples/${newExample}`))) {
+    console.log(chalk.red(`Not found examples/${newExample}`));
+    console.log('');
+    process.exit(1);
   }
-
-  await remove('workspace');
-  await copy(`examples/${example}`, 'workspace');
-  execSync('cd workspace');
-  execSync('yarn && yarn start', {
-    cwd: resolve(__dirname, 'workspace'),
-    stdio: 'inherit'
-  });
+  example = newExample;
 }
 
-dev();
+removeSync('workspace');
+copySync(`examples/${example}`, 'workspace');
+execSync('cd workspace');
+execSync('yarn && yarn start', {
+  cwd: resolve(__dirname, 'workspace'),
+  stdio: 'inherit'
+});
