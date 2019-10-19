fb0576aa54752c083752671474e5fa2ce2bda730
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-19 23:16:23 +0900

    Refactoring

--- packages/express/lib/core/ssr.tsx
+++ packages/express/lib/core/ssr.tsx
@@ -18,8 +18,8 @@ export default (props: SsrProps) => {
   const withHtml: boolean = 0 <= html.indexOf('html');
 
   let ssrId: string = 'default';
-  0 <= html.indexOf('emotion') && (ssrId = 'emotion');
   0 <= html.indexOf('mui') && (ssrId = 'mui');
+  0 <= html.indexOf('emotion') && (ssrId = 'emotion');
 
   switch (ssrId) {
     case 'mui':
