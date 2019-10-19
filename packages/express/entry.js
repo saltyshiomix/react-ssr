53a66f6bc7c83c197b128d2facc3ba474c685b9b
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-19 22:33:10 +0900

    Change ignoredYellowBox contents

--- packages/express/entry.js
+++ packages/express/entry.js
@@ -34,7 +34,7 @@ switch (ssrId) {
 
   case 'mui':
     // because Material UI uses "useLayoutEffect" so many times, suppress warnings
-    console.ignoredYellowBox = ['Warning: useLayoutEffect does nothing on the server, because its effect cannot be encoded into the server renderer\'s output format. This will lead to a mismatch between the initial, non-hydrated UI and the intended UI. To avoid this, useLayoutEffect should only be used in components that render exclusively on the client. See https://fb.me/react-uselayouteffect-ssr for common fixes.'];
+    console.ignoredYellowBox = ['Warning: useLayoutEffect does nothing on the server'];
 
     function MuiApp(props) {
       React.useEffect(() => {
