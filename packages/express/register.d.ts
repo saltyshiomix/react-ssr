c539e94b6a4169cc9c52c88531a0ce5f67453487
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-21 17:49:29 +0900

    Fix types

--- packages/express/register.d.ts
+++ packages/express/register.d.ts
@@ -1,5 +1,10 @@
 import express from 'express';
-import Config from './lib/core/config';
+
+interface Config {
+  viewsDir?: string;
+  cacheDir?: string;
+}
+
 
 declare function register(app: express.Application, config?: Config): Promise<void>;
 
