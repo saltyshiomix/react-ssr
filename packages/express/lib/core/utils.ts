5a62bdcb9701869db940a20744b62fd7b8609640
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-22 19:13:28 +0900

    test: inject @babel/register

--- packages/express/lib/core/utils.ts
+++ packages/express/lib/core/utils.ts
@@ -152,6 +152,29 @@ export const babelRequire = (file: string) => {
   // console.log(rawCode);
 
   const result = require("@babel/core").transform(`
+require('@babel/register')({
+  presets: [
+    require('@babel/preset-env'),
+    require('@babel/preset-react'),
+    require('@babel/preset-typescript'),
+  ],
+  plugins: [
+    require('babel-plugin-react-require'),
+    require('babel-plugin-css-modules-transform'),
+    require('@babel/plugin-syntax-dynamic-import'),
+    require('@babel/plugin-proposal-class-properties'),
+    [require('@babel/plugin-proposal-object-rest-spread'), {
+      useBuiltIns: true,
+    }],
+    require('@babel/plugin-transform-react-jsx'),
+    [require('@babel/plugin-transform-runtime'), {
+      corejs: 2,
+      helpers: true,
+      regenerator: true,
+      useESModules: false,
+    }],
+  ],
+});
 ${rawCode}
 `, {
     filename: file,
