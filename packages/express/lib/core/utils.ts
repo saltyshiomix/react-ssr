0c2de33cfed4f86b1df7746d8e65d4aebddcabfd
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-23 03:00:34 +0900

    debug

--- packages/express/lib/core/utils.ts
+++ packages/express/lib/core/utils.ts
@@ -108,21 +108,45 @@ export const readFileWithProps = (file: string, props: any) => {
 
 export const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
 
+// export const getBabelPresetsAndPlugins = () => {
+//   const presets = [
+//     require('@babel/preset-env'),
+//     require('@babel/preset-react'),
+//     require('@babel/preset-typescript'),
+//   ];
+//   const plugins = [
+//     require('babel-plugin-react-require'),
+//     require('babel-plugin-css-modules-transform'),
+//     require('@babel/plugin-syntax-dynamic-import'),
+//     require('@babel/plugin-proposal-class-properties'),
+//     [require('@babel/plugin-proposal-object-rest-spread'), {
+//       useBuiltIns: true,
+//     }],
+//     [require('@babel/plugin-transform-runtime'), {
+//       corejs: 2,
+//       helpers: true,
+//       regenerator: true,
+//       useESModules: false,
+//     }],
+//   ];
+//   return { presets, plugins };
+// };
+
 export const getBabelPresetsAndPlugins = () => {
   const presets = [
-    require('@babel/preset-env'),
-    require('@babel/preset-react'),
-    require('@babel/preset-typescript'),
+    '@babel/preset-env',
+    '@babel/preset-react',
+    '@babel/preset-typescript',
   ];
   const plugins = [
-    require('babel-plugin-react-require'),
-    require('babel-plugin-css-modules-transform'),
-    require('@babel/plugin-syntax-dynamic-import'),
-    require('@babel/plugin-proposal-class-properties'),
-    [require('@babel/plugin-proposal-object-rest-spread'), {
+    'babel-plugin-react-require',
+    'babel-plugin-css-modules-transform',
+    '@babel/plugin-syntax-dynamic-import',
+    '@babel/plugin-proposal-class-properties',
+    ['@babel/plugin-proposal-object-rest-spread', {
       useBuiltIns: true,
     }],
-    [require('@babel/plugin-transform-runtime'), {
+    ['@babel/plugin-transform-runtime', {
       corejs: 2,
       helpers: true,
       regenerator: true,
