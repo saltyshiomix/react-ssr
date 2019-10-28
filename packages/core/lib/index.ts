5e9d92c4b6b11afa1e374931ff4ddb115cbca6c6
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-29 06:52:00 +0900

    test

--- packages/core/lib/index.ts
+++ packages/core/lib/index.ts
@@ -1,6 +1,9 @@
+const _env = process.env.NODE_ENV;
 process.env.NODE_ENV = 'development';
 
 export * from './helpers/babel';
 export * from './helpers/core';
 export * from './render';
 export * from './webpack.config';
+
+process.env.NODE_ENV = _env;
