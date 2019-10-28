cf99035ca9e91a22a688a03979cb8fa29ff5e047
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-29 05:29:34 +0900

    Update examples/basic-blogging

--- examples/basic-blogging/views/index.jsx
+++ examples/basic-blogging/views/index.jsx
@@ -1,6 +1,12 @@
+import React from 'react';
+import Head from '@react-ssr/express/head';
+
 const Index = ({ posts }) => {
   return (
     <React.Fragment>
+      <Head>
+        <title>basic-blogging</title>
+      </Head>
       {posts.map((post, index) => {
         return (
           <p key={index}>
--- examples/basic-blogging/views/posts.jsx
+++ examples/basic-blogging/views/posts.jsx
@@ -1,5 +1,15 @@
+import React from 'react';
+import Head from '@react-ssr/express/head';
+
 const Post = ({ post }) => {
-  return <p>{post.body}</p>;
+  return (
+    <React.Fragment>
+      <Head>
+        <title>{post.body} - basic-blogging</title>
+      </Head>
+      <p>{post.body}</p>
+    </React.Fragment>
+  );
 };
 
 export default Post;
