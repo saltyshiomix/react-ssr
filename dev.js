17aaefd4350bc2dc7627f3818c247ab331deaa39
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-29 23:38:34 +0900

    Use local dependencies when testing examples/* #1

--- dev.js
+++ dev.js
@@ -1,15 +1,11 @@
-const {
-  existsSync,
-  copySync,
-  removeSync,
-} = require('fs-extra');
-const { resolve } = require('path');
+const fse = require('fs-extra');
+const path = require('path');
 const { execSync } = require('child_process');
 
 let example = 'basic-jsx';
 if (3 <= process.argv.length) {
   const newExample = process.argv[2];
-  if (!existsSync(resolve(__dirname, `examples/${newExample}`))) {
+  if (!fse.existsSync(path.join(__dirname, `examples/${newExample}`))) {
     console.log(`Not found examples/${newExample}`);
     console.log('');
     process.exit(1);
@@ -17,10 +13,24 @@ if (3 <= process.argv.length) {
   example = newExample;
 }
 
-removeSync('workspace');
-copySync(`examples/${example}`, 'workspace');
+fse.removeSync('workspace');
+
+fse.copySync(`examples/${example}`, 'workspace');
+const pkg = path.join(__dirname, 'workspace/package.json');
+const content = fse.readJSONSync(pkg);
+content.dependencies['@react-ssr/core'] = path.join(__dirname, 'packages/core');
+if (content.dependencies['@react-ssr/express']) {
+  content.dependencies['@react-ssr/express'] = path.join(__dirname, 'packages/express');
+}
+if (content.dependencies['@react-ssr/nestjs-express']) {
+  content.dependencies['@react-ssr/nestjs-express'] = path.join(__dirname, 'packages/nestjs-express');
+}
+fse.writeJSONSync(pkg, {...content}, {
+  spaces: 2,
+});
+
 execSync('cd workspace');
 execSync('yarn && yarn start', {
-  cwd: resolve(__dirname, 'workspace'),
+  cwd: path.join(__dirname, 'workspace'),
   stdio: 'inherit'
 });
