4937750df0614f27940fca51514838ca78c9b216
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-10-31 23:44:59 +0900

    Refactoring

--- packages/core/src/ssr/script.tsx
+++ packages/core/src/ssr/script.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 
 export default ({ script }: { script: string }) => {
-  const mountable = typeof window === 'undefined';
   return (
     <React.Fragment>
-      {mountable ? <script id="react-ssr-script" src={script}></script> : null}
+      {typeof window === 'undefined' ? <script id="react-ssr-script" src={script}></script> : null}
     </React.Fragment>
   );
 };
