2107f6da00798d1d4cefbe7827ba3d9a99e7a190
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-11-01 00:18:15 +0900

    Update examples

--- examples/basic-blogging/server.js
+++ examples/basic-blogging/server.js
@@ -1,32 +1,38 @@
-const express = require('@react-ssr/express');
+const express = require('express');
+const register = require('@react-ssr/express/register');
+
 const app = express();
 
-const posts = [
-  { id: 1, body: 'This is a first post.' },
-  { id: 2, body: 'This is a second post.' },
-  { id: 3, body: 'This is a last post.' },
-];
+(async () => {
+  await register(app);
 
-const findById = (id) => {
-  for (let i = 0; i < posts.length; i++) {
-    const post = posts[i];
-    if (post.id === parseInt(id, 10)) {
-      return post;
+  const posts = [
+    { id: 1, body: 'This is a first post.' },
+    { id: 2, body: 'This is a second post.' },
+    { id: 3, body: 'This is a last post.' },
+  ];
+  
+  const findById = (id) => {
+    for (let i = 0; i < posts.length; i++) {
+      const post = posts[i];
+      if (post.id === parseInt(id, 10)) {
+        return post;
+      }
     }
-  }
-  return undefined;
-};
-
-app.get('/', (req, res) => {
-  res.render('index', { posts });
-});
-
-app.get('/posts/:postId', (req, res) => {
-  const { postId } = req.params;
-  const post = findById(postId);
-  res.render('posts', { post });
-});
-
-app.listen(3000, () => {
-  console.log('> Ready on http://localhost:3000');
-});
+    return undefined;
+  };
+  
+  app.get('/', (req, res) => {
+    res.render('index', { posts });
+  });
+  
+  app.get('/posts/:postId', (req, res) => {
+    const { postId } = req.params;
+    const post = findById(postId);
+    res.render('posts', { post });
+  });
+  
+  app.listen(3000, () => {
+    console.log('> Ready on http://localhost:3000');
+  });
+})();
--- examples/with-jsx-material-ui/server.js
+++ examples/with-jsx-material-ui/server.js
@@ -1,13 +1,17 @@
-const express = require('@react-ssr/express');
+const express = require('express');
+const register = require('@react-ssr/express/register');
+
 const app = express();
 
-app.use(express.static('public'));
+(async () => {
+  await register(app);
 
-app.get('/', (req, res) => {
-  const user = { name: 'Material UI' };
-  res.render('index', { user });
-});
-
-app.listen(3000, () => {
-  console.log('> Ready on http://localhost:3000');
-});
+  app.get('/', (req, res) => {
+    const user = { name: 'Material UI' };
+    res.render('index', { user });
+  });
+  
+  app.listen(3000, () => {
+    console.log('> Ready on http://localhost:3000');
+  });
+})();
