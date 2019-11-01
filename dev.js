bcc6f3b06b935b34c5ba178d2d1f763fc20b2cd8
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-11-01 10:49:18 +0900

    Use "latest" when testing examples

--- dev.js
+++ dev.js
@@ -16,19 +16,6 @@ if (3 <= process.argv.length) {
 fse.removeSync('workspace');
 
 fse.copySync(`examples/${example}`, 'workspace');
-const pkg = path.join(__dirname, 'workspace/package.json');
-const content = fse.readJSONSync(pkg);
-content.dependencies['@react-ssr/core'] = path.join(__dirname, 'packages/core');
-if (content.dependencies['@react-ssr/express']) {
-  content.dependencies['@react-ssr/express'] = path.join(__dirname, 'packages/express');
-}
-if (content.dependencies['@react-ssr/nestjs-express']) {
-  content.dependencies['@react-ssr/nestjs-express'] = path.join(__dirname, 'packages/nestjs-express');
-}
-fse.writeJSONSync(pkg, {...content}, {
-  spaces: 2,
-});
-
 execSync('cd workspace');
 execSync('yarn && yarn start', {
   cwd: path.join(__dirname, 'workspace'),
