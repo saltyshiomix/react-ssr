f55793973699000555325208a755770050cae281
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-11-01 17:21:37 +0900

    test: suppressHydrationWarning

--- packages/core/src/ssr/default.tsx
+++ packages/core/src/ssr/default.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
-import ReactHtmlParser from 'react-html-parser';
+import ReactHtmlParser, { convertNodeToElement } from 'react-html-parser';
 import cheerio from 'cheerio';
 import {
   extractHeadElements,
@@ -31,7 +31,10 @@ export default function Ssr(props: SsrProps) {
     return (
       <html {...convertAttrToJsxStyle($('html').attr())}>
         <head>
-          {Title ? <Title /> : ReactHtmlParser($.html($('title')))}
+          {Title ? <Title /> : ReactHtmlParser($.html($('title')), { transform: (node, index, transform: any) => {
+            node.attribs!['suppressHydrationWarning'] = 'suppressHydrationWarning';
+            return convertNodeToElement(node, index, transform);
+          } })}
           {MetaDescription ? <MetaDescription /> : ReactHtmlParser($.html($('meta[name=description]')))}
           {ReactHtmlParser(meta)}
           {ReactHtmlParser(styles)}
