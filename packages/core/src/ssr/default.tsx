c3bea5066e2d10001011f53e6effe927dcb195fa
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-11-01 15:02:32 +0900

    debug: check collected head elements

--- packages/core/src/ssr/default.tsx
+++ packages/core/src/ssr/default.tsx
@@ -25,6 +25,8 @@ export default function Ssr(props: SsrProps) {
   const html = ReactDOMServer.renderToString(children);
   const withHtml = 0 <= html.toLowerCase().indexOf('html');
 
+  console.log(elements);
+
   // these must be called after ReactDOMServer.renderToString()
   let Title = undefined;
   let MetaDescription = undefined;
