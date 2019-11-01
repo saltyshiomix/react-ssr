281238e2630a408c6e2d6e5c5c3a157395e14630
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-11-01 16:18:39 +0900

    debug: check collected head elements

--- packages/core/lib/webpack/default.js
+++ packages/core/lib/webpack/default.js
@@ -1,146 +1,148 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import ReactDOMServer from 'react-dom/server';
+import ReactDOMServer from 'react-dom/server';
 // import PageEnhancer from './enhancer';
 import Page from '__REACT_SSR_PAGE__';
-import ReactHtmlParser from 'react-html-parser';
-import cheerio from 'cheerio';
+// import ReactHtmlParser from 'react-html-parser';
+// import cheerio from 'cheerio';
 
 const props = JSON.parse('__REACT_SSR_PROPS__');
-// const html = ReactDOMServer.renderToStaticMarkup(<Page {...props} />);
-// const withHtml = 0 <= html.indexOf('html');
-// const container = withHtml ? document : document.getElementById('react-ssr-root');
+const html = ReactDOMServer.renderToStaticMarkup(<Page {...props} />);
+const withHtml = 0 <= html.indexOf('html');
+const container = withHtml ? document : document.getElementById('react-ssr-root');
 
-const convertAttrToJsxStyle = attr => {
-  const jsxAttr = {};
-  const keys = Object.keys(attr);
-  for (let i = 0; i < keys.length; i++) {
-    let key = keys[i];
-    if (key === 'class') {
-      key = 'className';
-    }
-    if (0 <= key.indexOf('-')) {
-      if (!key.startsWith('data-')) {
-        key = key.replace(/-([a-z])/g, g => g[1].toUpperCase());
-      }
-    }
-    jsxAttr[key] = attr[keys[i]];
-  }
-  return jsxAttr;
-}
+ReactDOM.hydrate(<Page {...props} />, container);
 
-let _headElement = undefined;
+// const convertAttrToJsxStyle = attr => {
+//   const jsxAttr = {};
+//   const keys = Object.keys(attr);
+//   for (let i = 0; i < keys.length; i++) {
+//     let key = keys[i];
+//     if (key === 'class') {
+//       key = 'className';
+//     }
+//     if (0 <= key.indexOf('-')) {
+//       if (!key.startsWith('data-')) {
+//         key = key.replace(/-([a-z])/g, g => g[1].toUpperCase());
+//       }
+//     }
+//     jsxAttr[key] = attr[keys[i]];
+//   }
+//   return jsxAttr;
+// }
 
-const getHeadElement = child => {
-  if (typeof child === 'string' || child.type === 'script') {
-    return undefined;
-  }
-  if (typeof child.type === 'function') {
-    if (child.type.name.toLowerCase() === 'script') {
-      return undefined;
-    }
-    if (child.type.name.toLowerCase() === 'head') {
-      return child;
-    }
-    return getHeadElement(child.type(child.props));
-  }
-  React.Children.forEach(child.props.children, child => {
-    if (_headElement) {
-      return;
-    }
-    const __headElement = getHeadElement(child);
-    if (__headElement) {
-      _headElement = __headElement;
-      return;
-    }
-    if (typeof child.type === 'function' && child.type.name.toLowerCase() === 'head') {
-      _headElement = child;
-      return;
-    }
-  });
-  return _headElement;
-};
+// let _headElement = undefined;
 
-const extractHeadElements = element => {
-  const headElement = getHeadElement(element);
-  if (!headElement.props.children) {
-    return {
-      Title: undefined,
-      MetaDescription: undefined,
-    };
-  }
-  let Title = undefined;
-  let MetaDescription = undefined;
-  React.Children.forEach(headElement.props.children, child => {
-    if (child.type === 'title') {
-      Title = props => React.cloneElement(child, props);
-    }
-    if (child.type === 'meta' && child.props.name === 'description') {
-      MetaDescription = props => React.cloneElement(child, props);
-    }
-  });
-  return {
-    Title,
-    MetaDescription,
-  }
-};
+// const getHeadElement = child => {
+//   if (typeof child === 'string' || child.type === 'script') {
+//     return undefined;
+//   }
+//   if (typeof child.type === 'function') {
+//     if (child.type.name.toLowerCase() === 'script') {
+//       return undefined;
+//     }
+//     if (child.type.name.toLowerCase() === 'head') {
+//       return child;
+//     }
+//     return getHeadElement(child.type(child.props));
+//   }
+//   React.Children.forEach(child.props.children, child => {
+//     if (_headElement) {
+//       return;
+//     }
+//     const __headElement = getHeadElement(child);
+//     if (__headElement) {
+//       _headElement = __headElement;
+//       return;
+//     }
+//     if (typeof child.type === 'function' && child.type.name.toLowerCase() === 'head') {
+//       _headElement = child;
+//       return;
+//     }
+//   });
+//   return _headElement;
+// };
 
-const EnhancePage = element => props => {
-  const {
-    Title,
-    MetaDescription,
-  } = extractHeadElements(element);
+// const extractHeadElements = element => {
+//   const headElement = getHeadElement(element);
+//   if (!headElement.props.children) {
+//     return {
+//       Title: undefined,
+//       MetaDescription: undefined,
+//     };
+//   }
+//   let Title = undefined;
+//   let MetaDescription = undefined;
+//   React.Children.forEach(headElement.props.children, child => {
+//     if (child.type === 'title') {
+//       Title = props => React.cloneElement(child, props);
+//     }
+//     if (child.type === 'meta' && child.props.name === 'description') {
+//       MetaDescription = props => React.cloneElement(child, props);
+//     }
+//   });
+//   return {
+//     Title,
+//     MetaDescription,
+//   }
+// };
 
-  const html = ReactDOMServer.renderToString(<Page {...props} />);
-  const withHtml = 0 <= html.indexOf('html');
-  const container = withHtml ? document : document.getElementById('react-ssr-root');
+// const EnhancePage = element => props => {
+//   const {
+//     Title,
+//     MetaDescription,
+//   } = extractHeadElements(element);
 
-  if (withHtml) {
-    const $ = cheerio.load(html);
-    const meta = $.html($('head meta').filter((i, el) => $(el).attr('name') !== 'description'));
-    const styles = $.html($('head style'));
-    const body = $('body').html();
-    return [
-      container,
-      (
-        <html {...convertAttrToJsxStyle($('html').attr())}>
-          <head>
-            {Title ? <Title /> : ReactHtmlParser($.html($('title')))}
-            {MetaDescription ? <MetaDescription /> : ReactHtmlParser($.html($('meta[name=description]')))}
-            {ReactHtmlParser(meta)}
-            {ReactHtmlParser(styles)}
-          </head>
-          <body {...convertAttrToJsxStyle($('body').attr())}>
-            {body ? ReactHtmlParser(body) : null}
-            {/* <script src={script}></script> */}
-          </body>
-        </html>
-      ),
-    ];
-  } else {
-    return [
-      container,
-      (
-        <html>
-          <head>
-            {Title && <Title />}
-            {MetaDescription && <MetaDescription />}
-          </head>
-          <body>
-            <div id="react-ssr-root">
-              {element}
-            </div>
-            {/* <script src={script}></script> */}
-          </body>
-        </html>
-      ),
-    ];
-  }
-};
+//   const html = ReactDOMServer.renderToString(<Page {...props} />);
+//   const withHtml = 0 <= html.indexOf('html');
+//   const container = withHtml ? document : document.getElementById('react-ssr-root');
 
-const [container, EnhancedPage] = EnhancePage(Page)(props);
+//   if (withHtml) {
+//     const $ = cheerio.load(html);
+//     const meta = $.html($('head meta').filter((i, el) => $(el).attr('name') !== 'description'));
+//     const styles = $.html($('head style'));
+//     const body = $('body').html();
+//     return [
+//       container,
+//       (
+//         <html {...convertAttrToJsxStyle($('html').attr())}>
+//           <head>
+//             {Title ? <Title /> : ReactHtmlParser($.html($('title')))}
+//             {MetaDescription ? <MetaDescription /> : ReactHtmlParser($.html($('meta[name=description]')))}
+//             {ReactHtmlParser(meta)}
+//             {ReactHtmlParser(styles)}
+//           </head>
+//           <body {...convertAttrToJsxStyle($('body').attr())}>
+//             {body ? ReactHtmlParser(body) : null}
+//             {/* <script src={script}></script> */}
+//           </body>
+//         </html>
+//       ),
+//     ];
+//   } else {
+//     return [
+//       container,
+//       (
+//         <html>
+//           <head>
+//             {Title && <Title />}
+//             {MetaDescription && <MetaDescription />}
+//           </head>
+//           <body>
+//             <div id="react-ssr-root">
+//               {element}
+//             </div>
+//             {/* <script src={script}></script> */}
+//           </body>
+//         </html>
+//       ),
+//     ];
+//   }
+// };
 
-ReactDOM.hydrate(<EnhancedPage />, container);
+// const [container, EnhancedPage] = EnhancePage(Page)(props);
+
+// ReactDOM.hydrate(<EnhancedPage />, container);
 
 if (module.hot) {
   module.hot.accept();
--- packages/core/src/components/head.tsx
+++ packages/core/src/components/head.tsx
@@ -28,23 +28,23 @@ export default function Head({ children }: { children: React.ReactNode }) {
   // let Title = undefined;
   // let MetaDescription = undefined;
 
-  // const elements = React.Children.toArray(children) as React.ReactElement[];
-  // for (let i = 0; i < elements.length; i++) {
-  //   const element = elements[i];
-  //   switch (element.type) {
-  //     case 'title':
-  //       Title = (props: any) => React.cloneElement(element, props);
-  //       // useTitle(element.props.children);
-  //       break;
-  //     case 'meta':
-  //       if (element.props.name === 'description') {
-  //         MetaDescription = (props: any) => React.cloneElement(element, props);
-  //       }
-  //       // useMeta(element.props);
-  //       break;
-  //     default:
-  //       break;
-  //   }
-  // }
+  const elements = React.Children.toArray(children) as React.ReactElement[];
+  for (let i = 0; i < elements.length; i++) {
+    const element = elements[i];
+    switch (element.type) {
+      case 'title':
+        // Title = (props: any) => React.cloneElement(element, props);
+        useTitle(element.props.children);
+        break;
+      case 'meta':
+        // if (element.props.name === 'description') {
+        //   MetaDescription = (props: any) => React.cloneElement(element, props);
+        // }
+        useMeta(element.props);
+        break;
+      default:
+        break;
+    }
+  }
   return null;
 }
