d10a13c7c946fe29dd0e0df182905fc667f998b6
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-11-01 22:49:18 +0900

    debug: React.cloneElement

--- packages/core/lib/webpack/default.js
+++ packages/core/lib/webpack/default.js
@@ -5,23 +5,18 @@ import { hasHtml } from '__REACT_SSR_HELPERS__';
 
 const props = JSON.parse('__REACT_SSR_PROPS__');
 
-const PageComponent = React.forwardRef((props, ref) => <Page innerRef={ref} {...props} />);
+const Clone = React.cloneElement(Page, Object.assign(props, {
+  ref: node => {
+    this._input = node;
+    const { ref } = Page;
+    if (typeof ref === 'function') ref(node);
+    else if (ref) ref.current = node;
+  }
+}));
 
-function App(props) {
-  const { innerRef, ...rest } = props;
-  return (
-    <PageComponent
-      ref={innerRef}
-      {...rest}
-    />
-  );
-}
+const container = hasHtml(Clone) ? document : document.getElementById('react-ssr-root');
 
-const ref = React.createRef();
-
-const container = hasHtml(<App innerRef={ref} {...props} />) ? document : document.getElementById('react-ssr-root');
-
-ReactDOM.hydrate(<App innerRef={ref} {...props} />, container);
+ReactDOM.hydrate(<Page {...props} />, container);
 
 if (module.hot) {
   module.hot.accept();
