f5ebc9ab35da44702898fd0535eb8ac3d08d7904
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-11-01 14:38:02 +0900

    Disable express client cache

--- packages/core/src/optimize/development.ts
+++ packages/core/src/optimize/development.ts
@@ -77,6 +77,13 @@ export default async (app: express.Application): Promise<void> => {
   });
 
   app.set('etag', false);
+  app.use((req, res, next) => {
+    res.setHeader('Surrogate-Control', 'no-store');
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+    next();
+  });
 
   app.use('/sockjs-node*', proxyMiddleware);
 
