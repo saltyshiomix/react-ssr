60d80d9837dcf6a91aa5dc0d5e70b5ab183740c9
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-11-01 20:59:57 +0900

    Use original instance instead of React.cloneElement()

--- packages/core/lib/webpack/styled-components.js
+++ packages/core/lib/webpack/styled-components.js
@@ -4,7 +4,7 @@ import Page from '__REACT_SSR_PAGE__';
 import { hasHtml } from '__REACT_SSR_HELPERS__';
 
 const props = JSON.parse('__REACT_SSR_PROPS__');
-const container = hasHtml(React.cloneElement(Page, props)) ? document : document.getElementById('react-ssr-root');
+const container = hasHtml(<Page {...props} />) ? document : document.getElementById('react-ssr-root');
 
 ReactDOM.hydrate(<Page {...props} />, container);
 
