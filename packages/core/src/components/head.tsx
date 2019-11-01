f2cce448cfc1722123697a0d666d2370e48af582
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-11-01 15:32:46 +0900

    debug: check collected head elements

--- packages/core/src/components/head.tsx
+++ packages/core/src/components/head.tsx
@@ -1,5 +1,46 @@
 import React from 'react';
 
-export default function Head({ children }: { children?: React.ReactNode }) {
+const useTitle = (title: string) => {
+  React.useEffect(() => {
+    document.title = title;
+  }, []);
+};
+
+const useMeta = (attr: any) => {
+  const keys = Object.keys(attr);
+  let selector = 'meta';
+  for (let i = 0; i < keys.length; i++) {
+    const key = keys[i];
+    selector += `[${key}=${attr[key]}]`;
+  }
+
+  React.useEffect(() => {
+    const meta: HTMLMetaElement = document.querySelector(selector) || document.createElement('meta');
+    for (let i = 0; i < keys.length; i++) {
+      const key = keys[i];
+      meta.setAttribute(key, attr[key]);
+    }
+    document.getElementsByTagName('head')[0].appendChild(meta);
+  }, []);
+};
+
+export default function Head({ children }: { children: React.ReactNode }) {
+  const elements = React.Children.toArray(children) as React.ReactElement[];
+  for (let i = 0; i < elements.length; i++) {
+    const element = elements[i];
+
+    switch (element.type) {
+      case 'title':
+        useTitle(element.props.children);
+        break;
+
+      case 'meta':
+        useMeta(element.props);
+        break;
+
+      default:
+        break;
+    }
+  }
   return null;
 }
