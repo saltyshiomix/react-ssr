0f6c46dbe01a48b82a9b8c470b4bf7ae6c65c148
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-11-01 21:58:18 +0900

    debug: HOC

--- packages/core/lib/webpack/default.js
+++ packages/core/lib/webpack/default.js
@@ -5,24 +5,26 @@ import { hasHtml } from '__REACT_SSR_HELPERS__';
 
 const props = JSON.parse('__REACT_SSR_PROPS__');
 
-function WithComponent(Component) {
-  return class extends React.Component {
-    constructor(props) {
-      super(props);
-    }
-    render() {
-      return <Component {...this.props} />;
-    }
-  }
-}
+// function WithComponent(Component) {
+//   return class extends React.Component {
+//     constructor(props) {
+//       super(props);
+//     }
+//     render() {
+//       return <Component {...this.props} />;
+//     }
+//   }
+// }
 
-const EnhancedComponent = WithComponent(Page);
+// const EnhancedComponent = WithComponent(Page);
 
-const enhanced = new EnhancedComponent(props);
+// const enhanced = new EnhancedComponent(props);
 
-const container = hasHtml(enhanced) ? document : document.getElementById('react-ssr-root');
+const PageComponent = new Page(props);
 
-ReactDOM.hydrate(<Page {...props} />, container);
+const container = hasHtml(PageComponent) ? document : document.getElementById('react-ssr-root');
+
+ReactDOM.hydrate(PageComponent, container);
 
 if (module.hot) {
   module.hot.accept();
