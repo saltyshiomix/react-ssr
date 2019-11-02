4b6f3782d021c59273b37577274983cdd8725f0c
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-11-02 19:54:39 +0900

    Fix: "text content did not match"

--- packages/core/lib/webpack/emotion.js
+++ packages/core/lib/webpack/emotion.js
@@ -4,7 +4,26 @@ import Page from '__REACT_SSR_PAGE__';
 
 const props = JSON.parse('__REACT_SSR_PROPS__');
 
-ReactDOM.hydrate(<Page {...props} />, document.getElementById('react-ssr-root') || document);
+const root = document.getElementById('react-ssr-root');
+const markup = root ? root.innerHTML : document.documentElement.outerHTML;
+const container = root || document;
+
+function App(props) {
+  const [hydrate, setHydrate] = React.useState(false);
+
+  React.useEffect(() => {
+    setHydrate(true);
+  }, []);
+
+  // wait untill hooks called so that dynamic `Head` can work correctly
+  if (!hydrate) {
+    return ReactHtmlParser(markup);
+  }
+
+  return <Page {...props} />;
+}
+
+ReactDOM.hydrate(<App {...props} />, container);
 
 if (module.hot) {
   module.hot.accept();
--- packages/core/lib/webpack/material-ui.js
+++ packages/core/lib/webpack/material-ui.js
@@ -4,17 +4,31 @@ import Page from '__REACT_SSR_PAGE__';
 
 const props = JSON.parse('__REACT_SSR_PROPS__');
 
+const root = document.getElementById('react-ssr-root');
+const markup = root ? root.innerHTML : document.documentElement.outerHTML;
+const container = root || document;
+
 function App(props) {
+  const [hydrate, setHydrate] = React.useState(false);
+
   React.useEffect(() => {
+    setHydrate(true);
+
     const jssStyles = document.getElementById('jss-server-side');
     if (jssStyles) {
       jssStyles.parentNode.removeChild(jssStyles);
     }
   }, []);
+
+  // wait untill hooks called so that dynamic `Head` can work correctly
+  if (!hydrate) {
+    return ReactHtmlParser(markup);
+  }
+
   return <Page {...props} />;
 }
 
-ReactDOM.hydrate(<App {...props} />, document.getElementById('react-ssr-root') || document);
+ReactDOM.hydrate(<App {...props} />, container);
 
 if (module.hot) {
   module.hot.accept();
