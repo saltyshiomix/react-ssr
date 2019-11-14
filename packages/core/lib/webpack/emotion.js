f408d31ad9a4bbaedb199b1205086f228c5c43ae
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-11-14 15:50:40 +0900

    Fix: "Prop `className` did not match" (emotion) #4

--- packages/core/lib/webpack/emotion.js
+++ packages/core/lib/webpack/emotion.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { CacheProvider } from '@emotion/core';
+import createCache from '@emotion/cache';
 import Page from '__REACT_SSR_PAGE__';
 
 const props = JSON.parse('__REACT_SSR_PROPS__');
 
-ReactDOM.hydrate(<Page {...props} />, document.getElementById('react-ssr-root'));
+const cache = createCache();
+
+ReactDOM.render((
+  <CacheProvider value={cache}>
+    <Page {...props} />
+  </CacheProvider>
+), document.getElementById('react-ssr-root'));
 
 if (module.hot) {
   module.hot.accept();
--- packages/core/src/render/stringify/emotion.tsx
+++ packages/core/src/render/stringify/emotion.tsx
@@ -8,8 +8,27 @@ import {
 
 const Head = require('./head');
 
+const { CacheProvider } = require('@emotion/core');
+const emotionCache = require('@emotion/cache');
+const emotionServer = require('create-emotion-server');
+const createCache = emotionCache.default || emotionCache;
+const createEmotionServer = emotionServer.default || emotionServer;
+const cache = createCache();
+const { extractCritical } = createEmotionServer(cache);
+
 export default (app: React.ReactElement, script: string) => {
-  const html = ReactDOMServer.renderToString(app);
+  const {
+    html,
+    css,
+    ids,
+  } = extractCritical(
+    ReactDOMServer.renderToString(
+      <CacheProvider value={cache}>
+        {app}
+      </CacheProvider>
+    ),
+  );
+
   const $ = cheerio.load(html);
 
   return `
@@ -17,10 +36,13 @@ export default (app: React.ReactElement, script: string) => {
 <html${convertAttrToString($('html').attr())}>
   <head>
     ${getHeadHtml(Head.rewind())}
+    ${$.html($('style'))}
+    <style data-emotion-css="${ids.join(' ')}">${css}</style>
   </head>
   <body${convertAttrToString($('body').attr())}>
     <div id="react-ssr-root">${$('body').html() || ''}</div>
     <script src="${script}"></script>
+    ${$.html($('script'))}
   </body>
 </html>
 `;
