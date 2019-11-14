3c27f3df4af74d0bbbaba9c2f12e40215599d86f
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-11-14 16:46:52 +0900

    Fix: wrap html attributes with double quotation

--- packages/core/src/render/stringify/helpers.ts
+++ packages/core/src/render/stringify/helpers.ts
@@ -3,7 +3,7 @@ export const convertAttrToString = (attr: any) => {
   const keys = Object.keys(attr);
   for (let i = 0; i < keys.length; i++) {
     const key = keys[i];
-    results.push(` ${key}=${attr[key]}`);
+    results.push(` ${key}="${attr[key]}"`);
   }
   return results.join('');
 };
