75d03f0a6e9412a0e7fb28eb11a2c70ebb782975
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-12-07 09:10:36 +0900

    Remove comment codes

--- packages/core/lib/webpack/material-ui.js
+++ packages/core/lib/webpack/material-ui.js
@@ -4,16 +4,6 @@ import Page from '__REACT_SSR_PAGE__';
 
 const props = JSON.parse(__REACT_SSR_PROPS__);
 
-// function App(props) {
-//   React.useEffect(() => {
-//     const jssStyles = document.getElementById('jss-server-side');
-//     if (jssStyles) {
-//       jssStyles.parentNode.removeChild(jssStyles);
-//     }
-//   }, []);
-//   return <Page {...props} />;
-// }
-
 ReactDOM.hydrate(<Page {...props} />, document.getElementById('react-ssr-root'));
 
 if (module.hot) {
