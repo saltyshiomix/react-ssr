b4938e742a46fcc484b9ae3f509489654b5eb7d7
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-12-17 13:06:36 +0900

    Use @rollup/plugin-node-resolve

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,5 +1,5 @@
 import external from 'rollup-plugin-auto-external';
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 import commonjs from 'rollup-plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
