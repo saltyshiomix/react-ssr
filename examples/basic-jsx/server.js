5de61a9a9898c8f27527ad8b2e92de83d398dec1
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-12-17 20:27:30 +0900

    Remove unnecessary white spaces

--- examples/basic-jsx/server.js
+++ examples/basic-jsx/server.js
@@ -10,7 +10,7 @@ const app = express();
     const user = { name: 'World' };
     res.render('index', { user });
   });
-  
+
   app.listen(3000, () => {
     console.log('> Ready on http://localhost:3000');
   });
