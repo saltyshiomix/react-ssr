f39ff5bc8ca7d18345ef6cc6e8b60f33c4eccc07
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2019-12-17 09:26:59 +0900

    Add examples/basic-hmr-scss

---
+++ examples/basic-hmr-scss/views/index.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+import '../styles/index.scss';
+
+const Index = ({ user }) => {
+  const [message, setMessage] = useState('waiting...');
+
+  const onClick = () => setMessage('This is a react-ssr!');
+
+  return (
+    <React.Fragment>
+      <p>Hello {user.name}!</p>
+      <button onClick={onClick}>Click Me</button>
+      <p>Message from state: {message}</p>
+    </React.Fragment>
+  );
+};
+
+export default Index;
