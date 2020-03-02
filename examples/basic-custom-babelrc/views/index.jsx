39f8807db08ed33a5a1206479c7bc53bdc258c79
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2020-03-03 05:28:39 +0900

    Update all examples

--- examples/basic-custom-babelrc/views/index.jsx
+++ examples/basic-custom-babelrc/views/index.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
+import { Head } from '@react-ssr/express';
 
-interface IndexProps {
-  user: any;
-}
-
-const Index = ({ user }: IndexProps) => {
+const Index = ({ user }) => {
   const [message, setMessage] = React.useState('waiting...');
 
   const onClick = () => setMessage('This is a react-ssr!');
 
   return (
     <React.Fragment>
+      <Head>
+        <title>
+          custom-views - @react-ssr/express
+        </title>
+      </Head>
       <p>Hello {user.name}!</p>
       <button onClick={onClick}>Click Me</button>
       <p>Message from state: {message}</p>
