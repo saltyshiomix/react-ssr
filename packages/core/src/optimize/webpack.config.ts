4167a1d6e8ab8a7818665a4d1921bea6ae4a92ab
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2020-03-13 06:53:27 +0900

    Remove code comments//

--- packages/core/src/optimize/webpack.config.ts
+++ packages/core/src/optimize/webpack.config.ts
@@ -53,15 +53,6 @@ const prodConfig: webpack.Configuration = {
 };
 
 export const configureWebpack = (entry: webpack.Entry): webpack.Configuration => {
-  // fix import .scss files
-  // require.extensions['.scss'] = () => {};
-  // require.extensions['.scss'] = () => {
-  //   return;
-  // };
-  // require.extensions['.css'] = () => {
-  //   return;
-  // };
-
   let config: webpack.Configuration = {
     mode: 'development',
     entry,
