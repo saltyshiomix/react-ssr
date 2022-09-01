950d242d4e1bd3a59b8a917dfcc3f988c1a388e0
Shiono Yoshihide <shiono.yoshihide@gmail.com>
2022-09-01 22:33:59 +0900

    chore: Bump @types/*

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,6 +1,6 @@
 import external from 'rollup-plugin-auto-external';
 import resolve from '@rollup/plugin-node-resolve';
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import { terser } from 'rollup-plugin-terser';
 
